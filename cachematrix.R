bd0df8ba1ac7b018e44662e5183ded45b4fcd204
yachabach <yachabach@gmail.com>
2020-07-21 08:47:16 -0400

    Submission of programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,84 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates a cache location for a matrix and it's inverse.
+## It also defines functions to access both.  We do this 
+## in the environment where these functions are defined using the <<- operator.
+## This is normally the Global Environment.  Doing so ensures we don't lose the
+## value (and the computer effort) when we leave a function.  As
+## long as the user is in an environment where makeCacheMatrix is available, the 
+## cache will be available.
 
+makeCacheMatrix <- function(x = matrix()) {
+        
+        ## initialize a memory location for the inverse
+        i <- NULL
+        
+        ## Define the 'SetMatrix' function.  
+        ## 
+        ## y - matrix passed to the function
+        ## x - matrix location in the parent environment
+        ## i - inverse matrix location in the parent environment
+        ## 
+        ## This function uses the <<- operator to
+        ## put the passed matrix (y) into the parent frame memory location (x).  
+        ## Since we are 'Set'ting a new matrix, we have not yet calculated an 
+        ## inverse so the inverse cache location (i) is set to NULL
+        ## 
+        setMatrix <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        ## Define the 'getMatrix' funciton.  It simply returns the matrix (x).
+        ## 
+        getMatrix <- function() x
+        
+        ## Define the 'setInverse' function.  Inv is the already solved inverse
+        ## matrix that was passed to the function.  It uses the <<- operator to
+        ## store the result in the parant environment
+        ## 
+        setInverse <- function(Inv) i <<- Inv
+        
+        ## Define 'getInverse' function.  It simply returns 'i' which exists
+        ## in the parent environment and, therefore, acts as a cache
+        ## 
+        getInverse <- function() i
+        
+        ## Create the list of function vectors.  If 'vec' is the vector returned
+        ## when users called makevector, users access these functions,
+        ## and therefore the cache, with vec$setMatrix, vec$getMatrix, 
+        ## vec$setInverse, vec$getInverse.
+        ## 
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
+## cacheSolve retrieves the cached matrix inverse.  The function preserves
+## computer time by checking to see if the Inverse has already been calculated.
+## This is why we set the inverse to NULL when we create a new Matrix Cache or
+## set the cache to a new matrix.
+## 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if(!is.null(i)) {
+                
+                ## Inverse already exists because it is not NULL Return what we 
+                ## already assigned and quit the function
+                ## 
+                message("getting cached data")
+                return(i)
+        }
+        
+        ## Inverse does not exist so we have to use the computer power to
+        ## calculate it and send the result to the cache.
+        ## 
+        data <- x$getMatrix()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
